b0b3fde5cafa52175c1350b60b80c4c2cdfb6f80
kimtaedong <rlaxoehd4234@naver.com>
2023-01-31 18:55:33 +0900

    <TEST> 게시물 저장 테스트 Sucess

--- Project_Umbrella_main/src/test/java/com/umbrella/project_umbrella/domain/Post/PostRepositoryTest.java
+++ Project_Umbrella_main/src/test/java/com/umbrella/project_umbrella/domain/Post/PostRepositoryTest.java
@@ -1,10 +1,40 @@
 package com.umbrella.project_umbrella.domain.Post;
 
+import com.umbrella.project_umbrella.service.PostService;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PostRepositoryTest {
+
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void Save(){
+        String content = "content";
+        String title = "title";
+        String writer = "writer";
+
+        postRepository.save(Post.builder().
+                content(content).
+                title(title).
+                writer(writer).build());
+
+        List<Post> postList = postRepository.findAll();
+
+        Post post = postList.get(0);
+
+        Assertions.assertEquals(post.getTitle(),title);
+
+    }
 
 }
